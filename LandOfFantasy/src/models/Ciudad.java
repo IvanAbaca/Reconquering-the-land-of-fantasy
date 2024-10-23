3a321657e4c3f597a23d77ffb96353ba0aa2f554
IvanAbacaCS <ivan.abaca@wbdcontractor.com>
2024-10-23 13:27:18 -0300

    First Dijkstra

--- LandOfFantasy/src/models/Ciudad.java
+++ LandOfFantasy/src/models/Ciudad.java
@@ -5,4 +5,15 @@ import java.util.PriorityQueue;
 public class Ciudad {
 	public PriorityQueue<Unidad> ejercito;
 	public boolean aliado;
+	
+	public Ciudad(PriorityQueue<Unidad> ejercito, boolean aliado) {
+		this.ejercito = ejercito;
+		this.aliado = aliado;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Ciudad: " + (aliado ? "aliada" : "enemiga");
+	}
 }
