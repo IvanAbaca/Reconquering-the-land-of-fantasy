f8c8b4436e576058d4cf0221115c253ea5505b9c
Agustin-Zabalgoitia <aguszabalgoitia@gmail.com>
2024-10-25 11:17:39 -0300

    Añadido número de ciudad
    Clase Ciudad:
    -Añadido número de ciudad para ayudar con la identificación de las ciudades más adelante cuando se utilice el algoritmo para encontrar el mejor camino
    -Modificado el método toString() para mostrar el número de la ciudad

--- LandOfFantasy/src/models/Ciudad.java
+++ LandOfFantasy/src/models/Ciudad.java
@@ -5,15 +5,18 @@ import java.util.PriorityQueue;
 public class Ciudad {
 	public PriorityQueue<Unidad> ejercito;
 	public boolean aliado;
+	private int numeroNodo;
+	private static int numeroInstancia = 1;
 	
 	public Ciudad(PriorityQueue<Unidad> ejercito, boolean aliado) {
 		this.ejercito = ejercito;
 		this.aliado = aliado;
+		this.numeroNodo = numeroInstancia++;
 	}
 	
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return "Ciudad: " + (aliado ? "aliada" : "enemiga");
+		return "Ciudad "+ numeroNodo +": " + (aliado ? "aliada" : "enemiga");
 	}
 }
